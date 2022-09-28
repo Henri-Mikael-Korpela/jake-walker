930524fdc3d924308f5ec6a8ac1d65d6e30d7e42
Henri Korpela <henri.mikael.korpela@gmail.com>
2022-09-28 22:03:05 +0300

    Implemented print with custom delay that can be in milliseconds; changed story to print to use delay

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,52 @@
+#include <chrono>
 #include <cstdio>
+#include <cstdint>
+#include <cstring>
+#include <thread>
+
+typedef int32_t I32;
+
+void print(char const *value){
+    printf("%s", value);
+}
+void print(char const *value, I32 delayInMilliseconds){
+    auto len = strlen(value);
+
+    for(decltype(len) i = 0; i != len; ++i){
+        printf("%c", value[i]);
+        
+        if(i + 1 != len){
+            fflush(stdout);
+            std::this_thread::sleep_for(std::chrono::milliseconds(delayInMilliseconds));
+        }
+    }
+}
 
 int main(int argc, char* argv[]){
-    printf("JAKE WALKER\n\n");
+    print("JAKE WALKER\0", 200);
+    print("\n\n\0", 1000);
 
-    printf("It is dark. You cannot see anything. You can only feel something soft touching your face. It is sand. Your eyes are opening. Now you see more clearly, but it is still dark.\n\n");
+    print("It is dark. You cannot see anything. You can only feel something soft touching your face. It is sand. Your eyes are opening. Now you see more clearly, but it is still dark.\0", 75);
+    print("\n\n\0", 1000);
 
-    printf("You have survived. You can't remember what happened, but one thing is for certain: you are in a strange place. It is somewhat scary to lay on the sand in this strange place.\n\n");
+    print("You have survived. You can't remember what happened, but one thing is for certain: you are in a strange place. It is somewhat scary to lay on the sand in this strange place.\0", 75);
+    print("\n\n\0", 1000);
 
-    printf("You rise up to sit down. You look around. There is no one around. It seems you are alone - at least for now...\n\n");
+    print("You rise up to sit down. You look around. There is no one around. It seems you are alone - at least for now...\0", 75);
+    print("\n\n\0", 1000);
 
-    printf("You may not remember how you ended up here, but you sure remember your name. Your name is Jake Walker. You are a scientiest. You have done research in your life, but maybe what you are about to face on this island is something extraordinary.\n\n");
+    print("You may not remember how you ended up here, but you sure remember your name. Your name is Jake Walker. You are a scientiest. You have done research in your life, but maybe what you are about to face on this island is something extraordinary.\0", 75);
+    print("\n\n\0", 1000);
 
-    printf("Wait, an island? That's right, that is what you can see. You are on a coast, a vast ocean is before your very eyes. It is getting cold. You need a place to stay.\n\n");
-    
-    printf("What do you do?\n\n");
+    print("Wait, an island? That's right, that is what you can see. You are on a coast, a vast ocean is before your very eyes. It is getting cold. You need a place to stay.\0", 75);
+    print("\n\n\0", 1000);
 
-    printf("1. Stand up and start walking\n");
-    printf("2. Continue laying on the beach\n");
-    printf("\n");
+    print("What do you do?", 100);
+    print("\n\n\0", 1000);
+
+    print("1. Stand up and start walking\n");
+    print("2. Continue sitting on the beach\n");
+    print("\n");
 
     return 0;
 }
