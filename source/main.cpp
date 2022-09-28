45a84be3120cc25d8c9001799ebaa86fd9b8c5bb
Henri Korpela <henri.mikael.korpela@gmail.com>
2022-09-28 21:33:02 +0300

    Added description till the first question is asked

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,23 @@
 #include <cstdio>
 
 int main(int argc, char* argv[]){
-    printf("JAKE WALKER\n");
+    printf("JAKE WALKER\n\n");
+
+    printf("It is dark. You cannot see anything. You can only feel something soft touching your face. It is sand. Your eyes are opening. Now you see more clearly, but it is still dark.\n\n");
+
+    printf("You have survived. You can't remember what happened, but one thing is for certain: you are in a strange place. It is somewhat scary to lay on the sand in this strange place.\n\n");
+
+    printf("You rise up to sit down. You look around. There is no one around. It seems you are alone - at least for now...\n\n");
+
+    printf("You may not remember how you ended up here, but you sure remember your name. Your name is Jake Walker. You are a scientiest. You have done research in your life, but maybe what you are about to face on this island is something extraordinary.\n\n");
+
+    printf("Wait, an island? That's right, that is what you can see. You are on a coast, a vast ocean is before your very eyes. It is getting cold. You need a place to stay.\n\n");
+    
+    printf("What do you do?\n\n");
+
+    printf("1. Stand up and start walking\n");
+    printf("2. Continue laying on the beach\n");
+    printf("\n");
+
     return 0;
 }
