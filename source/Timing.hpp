8b8b923d968535faa433abd2fb44e264ab1a49e6
Henri Korpela <henri.mikael.korpela@gmail.com>
2022-09-29 10:26:02 +0300

    Added types #include for Timing.hpp

--- source/Timing.hpp
+++ source/Timing.hpp
@@ -1,5 +1,6 @@
 #ifndef _JAKE_HEADER_TIMING
 #define _JAKE_HEADER_TIMING
+#include "types.hpp"
 
 inline void delay(I32 const& delay_in_milliseconds){
     std::this_thread::sleep_for(std::chrono::milliseconds(delay_in_milliseconds));
