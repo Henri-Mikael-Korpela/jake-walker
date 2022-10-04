a6f10d91a83ec6bcc92280a663677796dcbf0dc3
Henri Korpela <henri.mikael.korpela@gmail.com>
2022-10-04 22:30:14 +0300

    Added story start part without question and answer options

--- source/main.rs
+++ source/main.rs
@@ -9,6 +9,22 @@ pub struct PrintSettings {
 mod callbacks {
     use PrintSettings;
 
+    pub fn normal(c: &char) -> PrintSettings {
+        match c {
+            '.' => PrintSettings {
+                visible: true,
+                delay: 350,
+            },
+            ':' => PrintSettings {
+                visible: true,
+                delay: 350,
+            },
+            _ => PrintSettings {
+                visible: true,
+                delay: 20,
+            },
+        }
+    }
     pub fn paragraph_change(_c: &char) -> PrintSettings {
         return PrintSettings {
             visible: true,
@@ -42,8 +58,32 @@ fn print(value: &str, callback: fn(c: &char) -> PrintSettings) {
     io::stdout().flush().unwrap();
 }
 
+mod actions {
+    use callbacks;
+    use print;
+
+    pub fn start_story() {
+        print("It is dark. You cannot see anything. You can only feel something soft touching your face. It is sand. Your eyes are opening. Now you see more clearly, but it is still dark.", callbacks::normal);
+        print("\n\n", callbacks::paragraph_change);
+
+        print("You have survived. You can't remember what happened, but one thing is for certain: you are in a strange place. It is somewhat scary to lay on the sand in this strange place.", callbacks::normal);
+        print("\n\n", callbacks::paragraph_change);
+
+        print("You rise up to sit down. You look around. There is no one around. It seems you are alone - at least for now...", callbacks::normal);
+        print("\n\n", callbacks::paragraph_change);
+
+        print("You may not remember how you ended up here, but you sure remember your name. Your name is Jake Walker. You are a scientiest. You have done research in your life, but maybe what you are about to face on this island is something extraordinary.", callbacks::normal);
+        print("\n\n", callbacks::paragraph_change);
+
+        print("Wait, an island? That's right, that is what you can see. You are on a coast, a vast ocean is before your very eyes. It is getting cold. You need a place to stay.", callbacks::normal);
+        print("\n\n", callbacks::paragraph_change);
+    }
+}
+
 fn main() {
     print("JAKE WALKER", callbacks::title);
     delay(500);
-    print("\n\n\0", callbacks::paragraph_change);
+    print("\n\n", callbacks::paragraph_change);
+
+    actions::start_story();
 }
