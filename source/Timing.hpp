7c5987d297c60122c6e46e6064c0c2425d0cf611
Henri Korpela <henri.mikael.korpela@gmail.com>
2022-10-05 11:31:38 +0300

    Removed C++ code

--- source/Timing.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _JAKE_HEADER_TIMING
-#define _JAKE_HEADER_TIMING
-#include "types.hpp"
-
-inline void delay(I32 const& delay_in_milliseconds){
-    std::this_thread::sleep_for(std::chrono::milliseconds(delay_in_milliseconds));
-}
-
-#endif // _JAKE_HEADER_TIMING
--- source/types.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef _JAKE_HEADER_TYPES
-#define _JAKE_HEADER_TYPES
-
-typedef int32_t I32;
-
-#endif // _JAKE_HEADER_TYPES
